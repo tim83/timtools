4dfb711404912b883a0bc1902174517d24f1e7db
Tim Mees <tim.mees83g@gmail.com>
2020-08-16 11:48:33 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+	name='timtools',
+	version='0.1',
+	packages=['timtools'],
+	url='',
+	license='',
+	author='***REMOVED*** Mees',
+	author_email='tim@mees.vip',
+	description='',
+	install_requires=[
+		"logging",
+	],
+)
---
+++ timtools/logger.py
@@ -0,0 +1,33 @@
+import logging
+
+logfile = "/tmp/python_default.log"
+
+
+def get_logger(name=__name__, verbose=False, filename=logfile):
+	# Gets or creates a logger
+	logger = logging.getLogger(name)
+	logger.setLevel(logging.DEBUG)
+
+	# define console handler and set formatter
+	console_handler = logging.StreamHandler()
+	formatter = logging.Formatter('%(name)s: %(message)s')
+	console_handler.setFormatter(formatter)
+	# set log level
+	if verbose:
+		console_handler.setLevel(logging.DEBUG)
+	else:
+		console_handler.setLevel(logging.NOTSET)
+
+	# add console handler to logger
+	logger.addHandler(console_handler)
+
+	# define file handler and set formatter
+	file_handler = logging.FileHandler(filename)
+	file_handler.setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+	file_handler.setFormatter(formatter)
+
+	# add file handler to logger
+	logger.addHandler(file_handler)
+
+	return logger
