eacf38dfd626fe06aebd7df33a1cd9f1a8995991
Tim Mees <tim.mees83g@gmail.com>
2020-09-10 12:15:11 +0200

    Add documentation to get_logger function

--- timtools/logger.py
+++ timtools/logger.py
@@ -1,22 +1,22 @@
 import logging
+import sys
 
 logfile = "/tmp/python_default.log"
 
 
-def get_logger(name=__name__, verbose=False, filename=logfile):
+def get_logger(name: str = __name__, verbose: bool = False, filename: str = logfile) -> logging.Logger:
 	# Gets or creates a logger
 	logger = logging.getLogger(name)
-	logger.setLevel(logging.DEBUG)
+	# logger.setLevel(logging.DEBUG)
 
 	# define console handler and set formatter
 	console_handler = logging.StreamHandler()
 	formatter = logging.Formatter('%(name)s: %(message)s')
 	console_handler.setFormatter(formatter)
 	# set log level
-	if verbose:
+	if verbose or "-v" in sys.argv[1:]:
 		console_handler.setLevel(logging.DEBUG)
-	else:
-		console_handler.setLevel(logging.WARN)
+		logging.basicConfig(level=logging.DEBUG)
 
 	# add console handler to logger
 	logger.addHandler(console_handler)
