4db96a0a9001a3f609edcaddfd4a042fd8a5f7ac
Tim Mees <tim.mees83g@gmail.com>
2020-09-27 17:38:56 +0200

    Fix multiple loglines for 1 command

--- build/lib/timtools/logger.py
+++ build/lib/timtools/logger.py
@@ -1,25 +1,29 @@
+#! /usr/bin/python3
+
 import logging
+import sys
 
-logfile = "/tmp/python_default.log"
+
+class LogConfig:
+	logfile: str = "/tmp/python_default.log"
 
 
-def get_logger(name=__name__, verbose=False, filename=logfile):
+def get_logger(name: str = __name__, verbose: bool = False, filename: str = LogConfig.logfile) -> logging.Logger:
 	# Gets or creates a logger
 	logger = logging.getLogger(name)
-	logger.setLevel(logging.DEBUG)
+	# logger.setLevel(logging.DEBUG)
 
 	# define console handler and set formatter
 	console_handler = logging.StreamHandler()
 	formatter = logging.Formatter('%(name)s: %(message)s')
 	console_handler.setFormatter(formatter)
 	# set log level
-	if verbose:
+	if verbose or "-v" in sys.argv[1:]:
 		console_handler.setLevel(logging.DEBUG)
-	else:
-		console_handler.setLevel(logging.WARN)
+		logging.basicConfig(level=logging.DEBUG)
 
 	# add console handler to logger
-	logger.addHandler(console_handler)
+	# logger.addHandler(console_handler)
 
 	# define file handler and set formatter
 	file_handler = logging.FileHandler(filename)
--- timtools/log.py
+++ timtools/log.py
@@ -3,27 +3,24 @@
 import logging
 import sys
 
+logging.basicConfig(format='%(name)s: %(message)s')
+
 
 class LogConfig:
 	logfile: str = "/tmp/python_default.log"
 
 
-def get_logger(name: str = __name__, filename: str = LogConfig.logfile) -> logging.Logger:
+def get_logger(name: str = __name__, verbose: bool = False, filename: str = LogConfig.logfile) -> logging.Logger:
 	# Gets or creates a logger
 	logger = logging.getLogger(name)
 	# logger.setLevel(logging.DEBUG)
 
-	# define console handler and set formatter
-	console_handler = logging.StreamHandler()
-	formatter = logging.Formatter('%(name)s: %(message)s')
-	console_handler.setFormatter(formatter)
 	# set log level
-	if "-v" in sys.argv[1:]:
-		console_handler.setLevel(logging.DEBUG)
+	if verbose or "-v" in sys.argv[1:]:
 		logging.basicConfig(level=logging.DEBUG)
 
 	# add console handler to logger
-	logger.addHandler(console_handler)
+	# logger.addHandler(console_handler)
 
 	# define file handler and set formatter
 	file_handler = logging.FileHandler(filename)
