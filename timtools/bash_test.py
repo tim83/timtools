eda6e83e8bf785cee88f536376bfa01e66bf0330
Tim Mees <tim.mees83g@gmail.com>
2020-11-27 00:19:28 +0100

    Add tests for bash

---
+++ timtools/bash_test.py
@@ -0,0 +1,25 @@
+import pytest
+import subprocess
+
+import bash
+
+test_str = "hoi"
+
+def test_bash_getoutput():
+	assert bash.get_output(["echo", test_str]) == test_str
+
+
+def test_bash_run():
+	# Check output of command
+	assert bash.run(["echo", test_str]) == ""
+	assert bash.run(["echo", test_str], capture_stdout=True) == test_str
+	assert bash.run(["echo", test_str], capture_stderr=True) == ""
+
+	# Check command that does not exist
+	with pytest.raises(FileNotFoundError):
+		bash.run(["thiscommanddoesnotexist"])
+
+	# Check failing command
+	with pytest.raises(subprocess.CalledProcessError):
+		bash.run(["false"])
+	assert bash.run(["false"], passable_exit_codes=[1]) == ""
