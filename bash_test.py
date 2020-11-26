a613accde957db46318450ce42e5ff2971b78acf
Tim Mees <tim.mees83g@gmail.com>
2020-11-27 00:46:29 +0100

    Clean up

--- bash_test.py
+++ bash_test.py
@@ -1,9 +1,11 @@
-import pytest
 import subprocess
+
+import pytest
 
 from timtools import bash
 
 test_str = "Hello World"
+
 
 def test_bash_getoutput():
 	assert bash.get_output(["echo", test_str]) == test_str
