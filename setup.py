722dc28d5d3414a84673f681791082c5331ed51b
Tim Mees <tim.mees83@gmail.com>
2021-02-15 00:15:51 +0100

    Fix including files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
 	name='timtools',
-	version='0.28.1',
+	version='0.28.2',
 	packages=['timtools'],
 	url='',
 	license='',
@@ -20,5 +20,6 @@ setup(
 		"google-auth-httplib2",
 		"google-auth-oauthlib",
 	],
+	include_package_data=True,
 	data_files=[('', ['timtools/client_secret.json'])]
 )
