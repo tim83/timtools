490132c18fd32e97f9f5bf2749bf67cf8369ea25
Tim Mees <tim.mees83@gmail.com>
2021-10-19 13:31:54 +0200

    Use absolute path

--- timtools/settings.py
+++ timtools/settings.py
@@ -5,7 +5,7 @@ PROJECT_DIR: str = os.path.dirname(__file__)
 CACHE_DIR: str = os.path.expanduser("~/.cache/timtools")
 CONFIG_DIR: str = os.path.expanduser("~/.config/timtools")
 if not os.path.isdir(CONFIG_DIR):
-	code_src_config_dir: str = os.path.expanduser("~/Programs/python/timtools/tim_config")
+	code_src_config_dir: str = os.path.expanduser("/home/tim/Programs/python/timtools/tim_config")
 	if os.path.isdir(code_src_config_dir):
 		CONFIG_DIR = code_src_config_dir
 
