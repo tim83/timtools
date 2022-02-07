cf7f90cbbd0baf780163cbc07f75ce2a0d7eb4b6
Tim Mees <tim.mees83@gmail.com>
2022-02-07 19:26:57 +0100

    Remove google

--- timtools/settings.py
+++ timtools/settings.py
@@ -2,7 +2,6 @@ import configparser
 import os
 
 PROJECT_DIR: str = os.path.dirname(__file__)
-CACHE_DIR: str = os.path.expanduser("~/.cache/timtools")
 CONFIG_DIR: str = os.path.expanduser("~/.config/timtools")
 if not os.path.isdir(CONFIG_DIR):
     code_src_config_dir: str = os.path.expanduser(
@@ -12,7 +11,6 @@ if not os.path.isdir(CONFIG_DIR):
         CONFIG_DIR = code_src_config_dir
 
 CONFIG_FILE: str = os.path.join(CONFIG_DIR, "timtools.ini")
-GOOGLE_CLIENT_SECRET_FILE: str = os.path.join(CONFIG_DIR, "google_client_secret.json")
 
 USER_CONFIG: configparser.ConfigParser = configparser.ConfigParser()
 USER_CONFIG.read(CONFIG_FILE)
