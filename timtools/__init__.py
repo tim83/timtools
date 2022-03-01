b229b58b422455bfc4ce7573b9bc111227f5b688
Tim Mees <tim.mees83@gmail.com>
2022-03-01 19:35:45 +0100

    fix(__init__.py): prevent circular import

--- timtools/__init__.py
+++ timtools/__init__.py
@@ -1,7 +1,2 @@
 #! /usr/bin/python3
 """Module containing a collection of useful tools"""
-
-from timtools import bash, log, multithreading, notify
-
-# Make sure the imports are not removed due to non-usage
-_ = [log, bash, notify, multithreading]
