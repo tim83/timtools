c6ebc02968d81e22cb9a4ee5e42fdc840f788b7d
Tim Mees <tim.mees83@gmail.com>
2022-10-26 18:51:23 +0200

    feat(settings.py): Allow using global config

--- timtools/settings.py
+++ timtools/settings.py
@@ -10,14 +10,16 @@ CACHE_DIR: Path = timtools.locations.get_user_cache_dir() / "timtools"
 if not CACHE_DIR.is_dir():
     CACHE_DIR.mkdir()
 
-CONFIG_DIR: Path = timtools.locations.get_user_config_dir() / "timtools"
-if not CONFIG_DIR.is_dir():
+CONFIG_DIR: Path
+user_config_dir = timtools.locations.get_user_config_dir() / "timtools"
+global_config_dir = Path("/etc/timtools")
+if not user_config_dir.is_dir():
+    CONFIG_DIR = user_config_dir
+elif global_config_dir.is_dir():
+    CONFIG_DIR = global_config_dir
+else:
     code_src_config_dir: Path = PROJECT_DIR / "tim_config"
-    if not code_src_config_dir.is_dir():
-        code_src_config_dir = Path("/home/tim/Programs/python/timtools/tim_config")
-
-    if code_src_config_dir.is_dir():
-        CONFIG_DIR = code_src_config_dir
+    CONFIG_DIR = code_src_config_dir
 
 CONFIG_FILE: Path = CONFIG_DIR / "timtools.ini"
 
